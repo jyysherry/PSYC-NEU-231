410e06a70e02d05261e1a8cfc681d1c08f7ed49b
Sherry Yueyi Jiang <sherryyueyijiang@Sherrys-MacBook-Pro.local>
2018-10-23 23:04:20 -0700

    updated

---
+++ first_modules.py
@@ -0,0 +1,37 @@
+def first_function(x):  
+    
+    if type(x) is list:
+    else:
+        print("please give me a list")
+        return None
+    if len(x) = 2:
+    else: 
+        print("please enter extractly two numeric values")
+        return None
+    if (type(x[0]) != float) or (type(x[1]) != float):
+            if x[0] <= x[1]:
+                print(x)
+            elif x[0] > x[1]:
+                print(x.sort())        
+    else: 
+        print("please enter extractly two values")
+    
+        
+        
+    
+    
+    
+#     if (type(x) != float) or (type(y) != float):
+#         print("please enter extractly two numeric values")
+#     if x < y:
+#         print(x, y)
+#     elif x >= y:
+#         print(y, x)
+#         return x,y
+
+def second_function(myList):
+    result = 1
+    for x in myList:
+        result = result * x
+    return result
+        
